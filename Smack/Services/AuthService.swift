4928150b0a4d80c29b40e63fb57481b6400b9891
tsoo6464 <tsoo6464@gmail.com>
2019-02-24 15:56:11 +0800

    add registerUser function

---
+++ Smack/Services/AuthService.swift
@@ -0,0 +1,70 @@
+//
+//  AuthService.swift
+//  Smack
+//
+//  Created by Nan on 2019/2/24.
+//  Copyright © 2019 nan. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService{
+    // 建立singleton
+    static let instance = AuthService()
+    // 使用userDefaults保存登入狀態等單純的基本數據
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn: Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken: String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let header = [
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString
+            { (response) in
+            
+            if response.result.error == nil {
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+        
+    }
+}
